35e588a833adb230f0b92f39977fd5764b27b8eb
captain-dj <whardig10@gmail.com>
2023-09-24 18:03:58 -0400

    V1.2 Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // user enters birthday
-        System.out.println("Enter your birthday [1,12]: ");
-        double birthMonth = scanner.nextDouble();
+        // puts up party choice
+        System.out.println("Party Affiliation Menu:");
+        System.out.println("D - Democrat");
+        System.out.println("R - Republican");
+        System.out.println("I - Independent");
+        System.out.print("Enter your choice (D/R/I): ");
 
-        // checks if input is valid
-        if (birthMonth >= 1 && birthMonth <= 12) {
-            System.out.println("Your birth day is " + birthMonth + " ");
-        } else {
-            System.out.println("This is a invalid input");
+        String choice = scanner.nextLine();
+
+        // Use of cascaded to determin what user chose
+        if (choice.equals("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        }
+        else if (choice.equals("R")) {
+            System.out.println("You get a Republican Elephant.");
+        }
+        else if (choice.equals("I")) {
+            System.out.println("You get a Person.");
+        }
+        else {
+            System.out.println("You get an other.");
         }
 
         scanner.close();
